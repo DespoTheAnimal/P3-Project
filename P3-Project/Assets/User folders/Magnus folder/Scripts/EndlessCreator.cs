c28f4366e25c0d3d0189febaf4a8faeefd7991a5
DespoTheAnimal <thedanishmagnus@gmail.com>
2022-10-14 11:06:04 +0200

    Moving ground works!

--- P3-Project/Assets/User folders/Magnus folder/Scripts/EndlessCreator.cs
+++ P3-Project/Assets/User folders/Magnus folder/Scripts/EndlessCreator.cs
@@ -1,32 +1,61 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class EndlessCreator : MonoBehaviour
 {
+    //The list of new gameobject in the scene
+    public static List<GameObject> movables = new List<GameObject>();
+    // The speed at which the ground is traveling
+    public  int speed = 5;
+
+    //The original Prefab of the ground
     public GameObject ground;
+    //The temporary name of the instantiated ground getting added to the list
+    public GameObject newGround;
+    // The startpoint of the plane
     [SerializeField]
-    private int planeStartPoint = 50;
+    private int planeStartPoint = 25;
+
+    //The time for which the plane will be created
+    float timer = 2f;
+    //The spawn position of each new plane
+    Vector3 spawnPosition;
 
     // Update is called once per frame
     void Start()
     {
-        StartCoroutine(PlaneGeneration());
+        //Start by adding the first plane to the list
+        movables.Add(GameObject.FindGameObjectWithTag("ILikeToMoveItMoveIt"));
+        spawnPosition = new Vector3(0, 0, 0 + planeStartPoint);
+        //StartCoroutine(PlaneGeneration());
     }
 
-    IEnumerator PlaneGeneration()
+    void Update()
     {
-        yield return new WaitForSeconds(2);
-        //Instantiate(ground, new Vector3(transform.position.x, transform.position.y, transform.position.z + planeStartPoint), Quaternion.identity);
-        for (int i = 0; i <= MagicNumberScript1.movables.Count; i++)
+        //This controlls the time for when a new plane will be instantiated 
+        timer -= Time.deltaTime;
+        if(timer <= 0)
         {
-            MagicNumberScript1.movables[i].transform.Translate(Vector3.back * MagicNumberScript1.speed * Time.deltaTime);
-            i++;
+            newGround = Instantiate(ground, spawnPosition, Quaternion.identity);
+            movables.Add(newGround);
+            planeStartPoint += 50;
+            timer = 2f;
         }
-        MagicNumberScript1.movables.Add(ground);
-        planeStartPoint += 50;
-        yield break;
-       
+
+      MovePlayer();
+    }
+
+    /// <summary>
+    /// Takes each element of movables and translates them according to the speed variable
+    /// </summary>
+    void MovePlayer()
+    {
+        foreach (GameObject go in movables)
+        {
+            go.transform.Translate(Vector3.back * (speed * Time.deltaTime));
+        }
     }
 }
--- P3-Project/Assets/User folders/Magnus folder/Scripts/MagicNumberScript1.cs
+++ P3-Project/Assets/User folders/Magnus folder/Scripts/MagicNumberScript1.cs
@@ -2,21 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagicNumberScript1 : MonoBehaviour
+/*public class MagicNumberScript1 : MonoBehaviour
 {
-    public static List<GameObject> movables = new List<GameObject>();
-    public static int speed = 5;
+    
+    // public static List<GameObject> movables = new List<GameObject>();
+    // public static int speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        movables.Add(this.gameObject);
+        foreach (GameObject gaem in GameObject.FindGameObjectsWithTag("ILikeToMoveItMoveIt"))
+        {
+            movables.Add(gaem);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+        // Update is called once per frame
+        void Update()
     {
         MovePlayer();
         Debug.Log(movables.Count);
+
     }
 
     void MovePlayer()
@@ -24,7 +29,12 @@ public class MagicNumberScript1 : MonoBehaviour
         for(int i = 0; i <= movables.Count; i++)
         {
           movables[i].transform.Translate(Vector3.back * (speed * Time.deltaTime));
+
+            if(movables[i].transform.parent != null)
+            {
+                movables.Remove(movables[i]);
+            }
             i++;
         }
     }
-}
+}*/
