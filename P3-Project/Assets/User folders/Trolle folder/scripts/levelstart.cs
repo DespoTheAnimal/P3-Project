f0fd4e408913f2f1ae4a023a74edccbabed3aa09
Trollemanden <mikkel_trolle@hotmail.com>
2022-10-19 10:46:32 +0200

    start sequence

---
+++ P3-Project/Assets/User folders/Trolle folder/scripts/levelstart.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelstart : MonoBehaviour
+{
+    public GameObject countDown3;
+    public GameObject countDown2;
+    public GameObject countDown1;
+    public GameObject countDownGO;
+    public GameObject fadeIn;
+    //public AudioSource GofX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Countsequence());
+    }
+
+    IEnumerator Countsequence()
+    {
+        yield return new WaitForSeconds(1);
+        countDown3.SetActive(true);
+        //GofX.Play();
+        yield return new WaitForSeconds(1);
+        countDown2.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countDown1.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countDownGO.SetActive(true);
+        //player can first move after the sound effects
+
+        //MovementKeyboard.canMove = true;
+
+    }
+}
+
