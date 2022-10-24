c31309e611ed213b9dcdfbf239683901687e13f8
Trollemanden <mikkel_trolle@hotmail.com>
2022-10-24 10:46:11 +0200

    ok

--- P3-Project/Assets/User folders/Magnus folder/Scripts/ObstacleCollision.cs
+++ P3-Project/Assets/User folders/Magnus folder/Scripts/ObstacleCollision.cs
@@ -2,25 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ObstacleCollision : MonoBehaviour
 {
-    public GameObject hit;
+    public GameObject Maincamera;
    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(Countsequence());
+            //Maincamera.GetComponent<Animator>().enabled = true;
             PlayerBehaviourMagnus.lives -= 1;
             // Her der skal være en linje kode med animatoren fra main camera så hver gang man bliver ramt så kommer der en animation
             EndlessCreator.movables.Remove(gameObject);
             Destroy(gameObject);
         }
-    }
-
-    IEnumerator Countsequence()
-    {
-        yield return new WaitForSeconds(1);
-        hit.SetActive(true);
     }
 }
