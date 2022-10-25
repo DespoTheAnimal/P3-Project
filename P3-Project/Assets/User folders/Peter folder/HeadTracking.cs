04d388ad882b1ff72e7ce1ed7513f1762f426ab2
PeterDaneholm <av28qm@student.aau.dk>
2022-10-25 09:12:20 +0200

    HeadTrakcUpdatea

--- P3-Project/Assets/User folders/Peter folder/HeadTracking.cs
+++ P3-Project/Assets/User folders/Peter folder/HeadTracking.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-//Made from tutorial: https://www.youtube.com/watch?v=m-CHTkMW_ho&ab_channel=Murtaza%27sWorkshop-RoboticsandAI
+//Made from tutorial: https://www.youtube.com/watch?v=m-CHTkMW_ho&ab_channel=Murtaza%27sWorkshop-RoboticsandAIk
 public class HeadTracking : MonoBehaviour
 {
     public UDPReceive uDPReceive;
     public GameObject player;
+    List<float> xList = new List<float>();
+    List<float> yList = new List<float>();
 
     private void Update()
     {
@@ -17,12 +20,20 @@ public class HeadTracking : MonoBehaviour
 
         string[] points = data.Split(',');
 
-        float x = float.Parse(points[0]) / 100;
-        float y = float.Parse(points[1]) / 100;
+        float x = (float.Parse(points[0])-320) / 100;
+        float y = (float.Parse(points[1])-240) / 100;
+        xList.Add(x);
+        yList.Add(y);
+
+        if (xList.Count > 10) { xList.RemoveAt(0); }
+        if (yList.Count > 10) { yList.RemoveAt(0); }
+
+        float xAverage = Queryable.Average(xList.AsQueryable());
+        float yAverage = Queryable.Average(yList.AsQueryable());
 
         Vector3 playerPos = player.transform.localPosition;
 
-        player.transform.localPosition = new Vector3(x, playerPos.y, playerPos.z);
+        player.transform.localPosition = new Vector3(xAverage, yAverage, playerPos.z);
 
     }
 }
