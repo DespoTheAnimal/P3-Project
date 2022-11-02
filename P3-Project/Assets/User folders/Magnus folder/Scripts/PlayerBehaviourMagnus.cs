a3f1b1db1b9f6818cd7d7b53067cf61ad219960a
Trollemanden <mikkel_trolle@hotmail.com>
2022-11-02 09:37:03 +0100

    DET FUNGER NOGENLUNDE, med mindre dfu dspioller det

--- P3-Project/Assets/User folders/Magnus folder/Scripts/PlayerBehaviourMagnus.cs
+++ P3-Project/Assets/User folders/Magnus folder/Scripts/PlayerBehaviourMagnus.cs
@@ -33,10 +33,13 @@ public class PlayerBehaviourMagnus : MonoBehaviour
         if (lives < 1)
         {
             Destroy(Hearts[0].gameObject);
+            Destroy(Hearts[1].gameObject);
+            Destroy(Hearts[2].gameObject);
         }
         else if (lives < 2)
         {
             Destroy(Hearts[1].gameObject);
+            Destroy(Hearts[2].gameObject);
         }
         else if (lives < 3)
         {
