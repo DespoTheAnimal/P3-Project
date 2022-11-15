67998ce306daee348a2471d53eeb831a935af80a
Trollemanden <mikkel_trolle@hotmail.com>
2022-11-15 09:47:31 +0100

    123

---
+++ P3-Project/Assets/FadeEnding.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeEnding : MonoBehaviour
+{
+    public GameObject Fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fade());
+    }
+
+    IEnumerator fade()
+    {
+        yield return new WaitForSeconds(6f);
+        Fade.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(4);
+
+    }
+}
