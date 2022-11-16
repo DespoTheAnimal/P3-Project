7e3006edfb3357cec61e536229e5389e99ee0c4c
Trollemanden <mikkel_trolle@hotmail.com>
2022-11-16 12:04:13 +0100

    Tænker noget af det fungere

---
+++ P3-Project/Assets/User folders/Trolle folder/Levertransition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levertransition : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Changetransition());
+    }
+
+    IEnumerator Changetransition()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(6);
+
+    }
+}
