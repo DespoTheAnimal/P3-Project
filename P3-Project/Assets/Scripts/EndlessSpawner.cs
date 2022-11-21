a9b2ec48275a0ae4451e451a00c51412bf0b2c6e
DespoTheAnimal <thedanishmagnus@gmail.com>
2022-11-21 12:11:05 +0100

    Added comments and other stuff

--- P3-Project/Assets/Scripts/EndlessSpawner.cs
+++ P3-Project/Assets/Scripts/EndlessSpawner.cs
@@ -7,13 +7,6 @@ public class EndlessSpawner : MonoBehaviour
     // Reference to player object
     PlayerBehaviour playerBehaviour;
 
-    // Spawns the next plane that spawns the walls
-    [SerializeField]
-    Collider spawnWall;
-
-    [SerializeField]
-    GameObject player;
-
     //The list of new gameobject in the scene
     public static List<GameObject> movables = new List<GameObject>();
     // The speed at which the ground is traveling
@@ -26,7 +19,7 @@ public class EndlessSpawner : MonoBehaviour
 
     // The startpoint of the plane
     private float planeStartPoint = 150f;        //74.5f; Original value for timer spawn
-    public static float timerForSpeed = 10;
+   // public static float timerForSpeed = 10;
 
     // The original wall for the level 
     public GameObject wall;
@@ -64,24 +57,24 @@ public class EndlessSpawner : MonoBehaviour
 
     void Update()
     {
-        //This controlls the time for when a new plane will be instantiated 
+        //This controls the time for when a new obstacle will be instantiated 
         timer -= Time.deltaTime;
-        timerForSpeed -= Time.deltaTime;
-        timerr -= Time.deltaTime;
+        timerr -= Time.deltaTime; // timer for the change of scene THIS IS A TEST
         if (timer <= 0)
         {
-            SpawnObstacles();
-            //spawnPosition.z += 50;
-            timer = originalTimer;
-            if (playerBehaviour.maxSpeed == 6)
+            SpawnObstacles(); //Spawns the obstacles 
+            timer = originalTimer; // sets the time back to it's original value 
+            if (playerBehaviour.maxSpeed == 6) //Speeds up the time for which the obstacles will spawn 
             {
                 originalTimer = 2f;
             }
         }
-        ChangeSceneSpawn();
-        Debug.Log(timerr);
+        ChangeSceneSpawn(); // Change of scene THIS IS A TEST 
     }
 
+    /// <summary>
+    /// Spawns the different obstacles randomly from the spawnposition
+    /// </summary>
     void SpawnObstacles()
     {
         if (timerr > 0)
@@ -104,35 +97,26 @@ public class EndlessSpawner : MonoBehaviour
             }
         }
         else return;
-        /*switch (Random.Range(0, obstacle.Count))
-        {
-            //Case 0 the blade, new vector represents the spawn position being mid on X, 6.5 on Y to fit the ceiling, and z in relation to the player
-            case 0:
-                newObstacle = Instantiate(obstacle[0], new Vector3(0, 10f, spawnPosition.z +25f), Quaternion.identity);
-                break;
-            //Case 1 the spiked log, new vector represents the spawn position being mid on X, 1 on Y and z in relation to the player
-            case 1:
-                newObstacle = Instantiate(obstacle[1], new Vector3(0, 1, spawnPosition.z), Quaternion.identity);
-                break;
-            //Case 2 the morningstar, new vector represents the spawn position being random on X axis, 10 on Y to fit the ceiling, and z in relation to the player
-            case 2:
-                newObstacle = Instantiate(obstacle[2], new Vector3(Random.Range(-4f, 4), 10, spawnPosition.z), Quaternion.identity);
-                break;
-
-        }*/
     }
 
+
+    /// <summary>
+    /// Spawns the walls of the world as well as the ground the player walks on 
+    /// </summary>
     public void SpawnWall()
     {
         { 
             newGround = Instantiate(ground, new Vector3(spawnPosition.x,spawnPosition.y, spawnPosition.z + 24.5f), Quaternion.identity);
             newWall = Instantiate(wall, spawnPosition, Quaternion.identity);
             spawnPosition.z += 50;
-            //spawnWall.transform.position = new Vector3(spawnWall.transform.position.x, spawnWall.transform.position.y, spawnPosition.z);
         }
         
     }
 
+
+    /// <summary>
+    /// Changes the instantiated prefrab, once a timer has reached zero, to prefab that changes the level
+    /// </summary>
     void ChangeSceneSpawn()
     {
         if (timerr <= 0)
