91fe0d783c45e513ece8db0bf985037c5cb45caf
PeterDaneholm <av28qm@student.aau.dk>
2022-11-22 12:24:01 +0100

    LeverpuzzleiSNowWorkingDespite7millionifstatementsandwhatnot

--- P3-Project/Assets/User folders/Magnus folder/Scripts/LeverPuzzle.cs
+++ P3-Project/Assets/User folders/Magnus folder/Scripts/LeverPuzzle.cs
@@ -3,10 +3,19 @@ using Input = UnityEngine.Input;
 using UnityEngine.SceneManagement;
 using UnityEngine.Animations;
 using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
 
 public class LeverPuzzle : MonoBehaviour
 {
+    public GameObject uDPReceive;
+    private GameObject player;
+    List<float> xList = new List<float>();
+    List<float> yList = new List<float>();
+    public float xPosAdjust = 320;
+    public float yPosAdjust = 400;
     public float offset = 3f;
+    public bool isHeadTrack = true;
 
     public int lever1;
     public int lever2;
@@ -24,6 +33,12 @@ public class LeverPuzzle : MonoBehaviour
     [SerializeField] public GameObject leverb2;
     [SerializeField] public GameObject leverb3;
 
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        uDPReceive = GameObject.FindGameObjectWithTag("Server");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,10 +55,14 @@ public class LeverPuzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 cursorPos = Input.mousePosition;
-        cursorPos.z = offset;
-        transform.position = Camera.main.ScreenToWorldPoint(cursorPos);
+        if (isHeadTrack == true)
+        {
+            HeadControl();
+        }
+        else
+        {
+            MouseControl();
+        }
 
         if(lever1 == leverCheck1 &&  lever2 == leverCheck2 && lever3 == leverCheck3)
         {
@@ -60,5 +79,36 @@ public class LeverPuzzle : MonoBehaviour
         Debug.Log(leverCheck2);
         Debug.Log(leverCheck3);
 
+    }
+
+    private void MouseControl()
+    {
+        Vector3 cursorPos = Input.mousePosition;
+        cursorPos.z = offset;
+        transform.position = Camera.main.ScreenToWorldPoint(cursorPos);
+    }
+    private void HeadControl()
+    {
+        string data = uDPReceive.GetComponent<UDPReceive>().data;
+        //The two below lines are removing the brackets in the first and last place
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length - 1, 1);
+
+        string[] points = data.Split(',');
+
+        float x = (xPosAdjust - float.Parse(points[0])) / 100;
+        float y = (yPosAdjust - float.Parse(points[1])) / 100;
+        xList.Add(x);
+        yList.Add(y);
+
+        if (xList.Count > 10) { xList.RemoveAt(0); }
+        if (yList.Count > 10) { yList.RemoveAt(0); }
+
+        float xAverage = Queryable.Average(xList.AsQueryable());
+        float yAverage = Queryable.Average(yList.AsQueryable());
+        Vector3 playerPos = new Vector3(Mathf.Clamp(xAverage, -4f, 4f), player.transform.position.y, player.transform.position.z);
+
+        //player.transform.localPosition = new Vector3(xAverage, playerPos.y, playerPos.z);
+        player.transform.localPosition = new Vector3(xAverage, yAverage, 15.95f);
     }
 }
